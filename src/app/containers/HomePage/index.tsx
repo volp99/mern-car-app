f1afa5f79c32b256afd5ed0de96d129cc34c6c97
volp99 <stefanovolpatti@yahoo.it>
2021-12-13 02:31:46 +0100

    set the Navbar

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
+import { Navbar } from "../../components/navbar";
 
 const PageContainer = styled.div`
   ${tw`flex flex-col w-full h-full items-center overflow-x-hidden`}
 `;
+
 export function HomePage() {
   return (
     <PageContainer>
-      <h1>Home Page</h1>
+      <Navbar />
     </PageContainer>
   );
 }
